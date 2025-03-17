ba50dbf231af73d7630dddeaa5510b3d90159ceb
adeel <adeelpv360@gmail.com>
2025-03-17 20:24:33 +0530

    Added solution for Two Sum

---
+++ challenge/two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_indices = {}
+        for i,num in enumerate(nums):
+            difference = target - num
+            if difference in num_indices:
+                return [num_indices[difference], i]
+            num_indices[num]=i
