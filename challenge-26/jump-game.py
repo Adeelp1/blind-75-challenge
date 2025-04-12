2db6eb552f5a4964e725d8733a60a7d0882efbd4
adeel <adeelpv360@gmail.com>
2025-04-12 15:04:00 +0530

    added solution for jump-game

---
+++ challenge-26/jump-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        i = 0
+        for n in nums:
+            if i < 0:
+                return False
+            elif n > i:
+                i = n
+            i -= 1
+        return True
+    
+# TC : O(N)
+# SC : O(1)
