9fba0b659ed4f2deed86ecc08943cae195f86d57
adeel <adeelpv360@gmail.com>
2025-04-18 21:19:29 +0530

    solution added for find-building-where-alice-and-bob-can-meet

---
+++ challenge-32/find-building-where-alice-and-bob-can-meet.py
@@ -0,0 +1,21 @@
+class Solution:
+    def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:
+        def leftmostBuildingQueries(self, A, queries):
+                que = [[] for a in A]
+                h = []
+                res = [-1] * len(queries)
+                for qi, (i, j) in enumerate(queries):
+                    if cmp(i, j) == cmp(A[i], A[j]):
+                        res[qi] = max(i, j)
+                    else:
+                        que[max(i, j)].append([max(A[i], A[j]), qi])
+                for i, a in enumerate(A):
+                    while h and h[0][0] < a:
+                        res[heappop(h)[1]] = i
+                    for q in que[i]:
+                        heappush(h, q)
+                return res
+
+# TC : O(qlogq)
+# SC : O(Q)
+# where q is size of query
