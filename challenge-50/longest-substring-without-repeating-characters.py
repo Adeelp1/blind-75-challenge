cb46f727d2c39540c0bc43c3cf464ba8218775af
adeel <adeelpv360@gmail.com>
2025-05-06 21:26:44 +0530

    solution added for longest-substring-without-repeating-characters

---
+++ challenge-50/longest-substring-without-repeating-characters.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        visited = set()
+        left = 0
+        max_len = 0
+        for right in range(len(s)):
+            while s[right] in visited:
+                visited.remove(s[left])
+                left += 1
+            
+            visited.add(s[right])
+            max_len = max(max_len, right - left+1)
+        
+        return max_len
+
+# TC : O(N)
+# SC : O(min(m, n))
+# where m = len(s) and n = len(visited)
